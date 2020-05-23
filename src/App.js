b274ae960547902f0bbb2f3325fb7159bdb977b2
Adam Pogwizd <adampog77@gmail.com>
2020-05-23 18:50:57 -0500

    randomly generate primary objective. Track progress

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import HexRow from './HexRow'
 import Buffer from './Buffer'
+import Objective from './Objective'
 import './App.css';
 
 class App extends React.Component {
   state = this.initialState()
 
   initialState() {
+    const board = this.generateBoard();
+    const primary = this.generatePrimaryObjective(board);
+
     return {
-      board: this.generate_board(),
+      board: board,
       highlightRow: 0,
       highlightCol: null,
-      buffer: []
+      buffer: [],
+      primaryObjective: primary,
+      primaryObjectiveProgress: primary.slice().reverse()
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.primaryObjectiveProgress.length === 0) {
+      console.log('Data retrieved')
+    } else {
+      console.log(this.state.primaryObjectiveProgress)
     }
   }
 
@@ -19,11 +33,11 @@ class App extends React.Component {
     this.setState(this.initialState())
   }
 
-  generate_board() {
+  generateBoard() {
     let hexValues = Array(5).fill().map(() => this.randomHex())
 
-    return Array(5).fill().map((_,i) =>
-      Array(5).fill().map((_,j) =>
+    return Array(5).fill().map(() =>
+      Array(5).fill().map(() =>
         hexValues[Math.floor(Math.random() * 5)]
       )
     )
@@ -36,22 +50,24 @@ class App extends React.Component {
       .padStart(2,'0')
   }
 
+  generatePrimaryObjective(board) {
+    let row1 = Math.floor(Math.random() * 5)
+    let col1 = Math.floor(Math.random() * 5)
+    let row2 = Math.floor(Math.random() * 5)
+    let col2 = Math.floor(Math.random() * 5)
+
+    return [board[row1][col1], board[row2][col2]]
+  }
+
   hexClick(row, col) {
     const board = this.state.board.slice();
     const buffer = this.state.buffer.slice();
-    let highlightRow = this.state.highlightRow;
-    let highlightCol = this.state.highlightCol;
-
     buffer.push(board[row][col]);
-    board[row][col] = null;
 
-    if (this.state.highlightRow != null) {
-      highlightRow = null;
-      highlightCol = col;
-    } else {
-      highlightRow = row;
-      highlightCol = null;
-    }
+    const [highlightRow, highlightCol] = this.refreshHighlights(row, col)
+    let primaryObjectiveProgress = this.updatePrimaryObjectiveProgress(buffer)
+
+    board[row][col] = null;
 
     if (buffer.length === 7) {
       this.reset();
@@ -60,14 +76,49 @@ class App extends React.Component {
         board,
         highlightRow,
         highlightCol,
-        buffer
+        buffer,
+        primaryObjectiveProgress
       })
     }
+  }
+
+  refreshHighlights(row, col) {
+    if (this.state.highlightRow != null) {
+      return [null, col]
+    } else {
+      return [row, null]
+    }
+  }
+
+
+  updatePrimaryObjectiveProgress(buffer) {
+    let primaryObjectiveProgress = this.state.primaryObjectiveProgress.slice()
+    const [selection] = buffer.slice(-1)
+    const [currentGoal] = primaryObjectiveProgress.slice(-1)
+
+    if (primaryObjectiveProgress.length === 0) {
+      return primaryObjectiveProgress
+    }
+    else if (selection === currentGoal) {
+      primaryObjectiveProgress.pop()
+    } else {
+      primaryObjectiveProgress = this.state.primaryObjective.slice().reverse()
+    }
+
+    return primaryObjectiveProgress
   }
 
   render() {
     return (
       <div className="App">
+        <div className='primaryObjective'>
+          <Objective
+            objective={this.state.primaryObjective}
+            progress={this.state.primaryObjectiveProgress}
+            >
+          </Objective>
+        </div>
+
         <table>
           <tbody>
             {
@@ -85,6 +136,7 @@ class App extends React.Component {
             }
           </tbody>
         </table>
+
         <Buffer
           buffer={this.state.buffer}
           size={6}
---
+++ src/Objective.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Objective.css';
+
+function Objective(props) {
+  return (
+    <div className="Objective">
+      {
+        props.objective.map((value,i) => {
+            let className = props.progress.includes(value) ? '' : 'objectiveHighlight'
+
+            return(
+              <div className={"ObjectiveTextContainer " + className} key={i}>
+                <p key={i}>{value}</p>
+              </div>
+            )
+        })
+      }
+    </div>
+  );
+}
+
+export default Objective;
